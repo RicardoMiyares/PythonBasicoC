5665d39b193aa86b30ee6ca4073dc599875f89dd
RicardoMiyares <miyaresricardo@gmail.com>
2020-11-06 13:50:44 -0600

    ht1

--- hojadetrabajo.py
+++ hojadetrabajo.py
@@ -1,13 +1,14 @@
 def jugar(intento=1):
     respuesta = input("De que color es la fruta?: \n")
     if respuesta != "naranja":
-        if intento <3:
+        if intento <3: 
             print("\n Fallaste, intentalo de nuevo")
             intento +=1
             jugar(intento) #Llamada recursiva
         else:
-            print("\n Perdiste")
+            print("\n Perdiste, intentalo de nuevo")
     else:
-        print("\n Ganaste")
+        print("\n Ganaste, eres lo maximo!")
 
 jugar()
+
